221f9c55e0e6f53d32060321c5fb160d4383afbc
Martin Zapata <martinchooooooo@gmail.com>
2013-04-11 23:28:03 +1000

    Updated dependenices on vmfest

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [vmfest "0.2.4-beta.5"]
+                 [vmfest "0.2.8"]
                  ;; the following are for logging purposes
                  [ch.qos.logback/logback-core "1.0.1"]
                  [ch.qos.logback/logback-classic "1.0.1"]])
